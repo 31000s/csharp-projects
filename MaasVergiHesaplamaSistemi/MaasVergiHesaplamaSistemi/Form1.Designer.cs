c6c38a58045d86ab7e4793b04fdff18885ad295d
Burak <64856839+31000s@users.noreply.github.com>
2022-11-12 19:21:27 +0300

    Add files via upload

---
+++ MaasVergiHesaplamaSistemi/MaasVergiHesaplamaSistemi/Form1.Designer.cs
@@ -0,0 +1,434 @@
+﻿namespace MaasVergiHesaplamaSistemi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Person1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.maasl1 = new System.Windows.Forms.Label();
+            this.maasl2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.Person2 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.maasl3 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.Person3 = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.toplami = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.normalMaaslar = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(50, 325);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 25);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Toplamını Hesapla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 113);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Maaş:";
+            // 
+            // Person1
+            // 
+            this.Person1.Location = new System.Drawing.Point(83, 110);
+            this.Person1.Name = "Person1";
+            this.Person1.Size = new System.Drawing.Size(100, 20);
+            this.Person1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(47, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Ad: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Soyad: ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(83, 42);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 11;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(83, 76);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(91, 146);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Sonuç: ";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(83, 180);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Hesapla";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // maasl1
+            // 
+            this.maasl1.AutoSize = true;
+            this.maasl1.Location = new System.Drawing.Point(142, 146);
+            this.maasl1.Name = "maasl1";
+            this.maasl1.Size = new System.Drawing.Size(19, 13);
+            this.maasl1.TabIndex = 12;
+            this.maasl1.Text = "....";
+            // 
+            // maasl2
+            // 
+            this.maasl2.AutoSize = true;
+            this.maasl2.Location = new System.Drawing.Point(317, 146);
+            this.maasl2.Name = "maasl2";
+            this.maasl2.Size = new System.Drawing.Size(19, 13);
+            this.maasl2.TabIndex = 21;
+            this.maasl2.Text = "....";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(258, 181);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 19;
+            this.button3.Text = "Hesapla";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(266, 146);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 13);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "Sonuç: ";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(258, 76);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 17;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(258, 42);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 20;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(205, 79);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(43, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Soyad: ";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(222, 45);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(26, 13);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Ad: ";
+            // 
+            // Person2
+            // 
+            this.Person2.Location = new System.Drawing.Point(258, 110);
+            this.Person2.Name = "Person2";
+            this.Person2.Size = new System.Drawing.Size(100, 20);
+            this.Person2.TabIndex = 14;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(202, 113);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(36, 13);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "Maaş:";
+            // 
+            // maasl3
+            // 
+            this.maasl3.AutoSize = true;
+            this.maasl3.Location = new System.Drawing.Point(501, 146);
+            this.maasl3.Name = "maasl3";
+            this.maasl3.Size = new System.Drawing.Size(19, 13);
+            this.maasl3.TabIndex = 30;
+            this.maasl3.Text = "....";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(442, 181);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 28;
+            this.button4.Text = "Hesapla";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(450, 146);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(44, 13);
+            this.label10.TabIndex = 27;
+            this.label10.Text = "Sonuç: ";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(442, 76);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 26;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(442, 42);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 29;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(389, 79);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(43, 13);
+            this.label11.TabIndex = 25;
+            this.label11.Text = "Soyad: ";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(406, 45);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(26, 13);
+            this.label12.TabIndex = 24;
+            this.label12.Text = "Ad: ";
+            // 
+            // Person3
+            // 
+            this.Person3.Location = new System.Drawing.Point(442, 110);
+            this.Person3.Name = "Person3";
+            this.Person3.Size = new System.Drawing.Size(100, 20);
+            this.Person3.TabIndex = 23;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(386, 113);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(36, 13);
+            this.label13.TabIndex = 22;
+            this.label13.Text = "Maaş:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(56, 294);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(84, 13);
+            this.label5.TabIndex = 31;
+            this.label5.Text = "Güncel Maaşlar:";
+            // 
+            // toplami
+            // 
+            this.toplami.AutoSize = true;
+            this.toplami.Location = new System.Drawing.Point(145, 294);
+            this.toplami.Name = "toplami";
+            this.toplami.Size = new System.Drawing.Size(13, 13);
+            this.toplami.TabIndex = 32;
+            this.toplami.Text = "..";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(56, 268);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(47, 13);
+            this.label14.TabIndex = 33;
+            this.label14.Text = "Maaşlar:";
+            // 
+            // normalMaaslar
+            // 
+            this.normalMaaslar.AutoSize = true;
+            this.normalMaaslar.Location = new System.Drawing.Point(110, 268);
+            this.normalMaaslar.Name = "normalMaaslar";
+            this.normalMaaslar.Size = new System.Drawing.Size(13, 13);
+            this.normalMaaslar.TabIndex = 34;
+            this.normalMaaslar.Text = "..";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(488, 331);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(99, 13);
+            this.linkLabel1.TabIndex = 35;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "github.com/31000s";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(615, 371);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.normalMaaslar);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.toplami);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.maasl3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.Person3);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.maasl2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.Person2);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.maasl1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Person1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Maaş Vergi Sistemi";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Person1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label maasl1;
+        private System.Windows.Forms.Label maasl2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox Person2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label maasl3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox Person3;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label toplami;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label normalMaaslar;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ MaasVergiHesaplamaSistemi/MaasVergiHesaplamaSistemi/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MaasVergiHesaplamaSistemi
+
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+
+            if (Person1.Text == "" || Person2.Text == "" || Person3.Text == "")
+            {
+
+                MessageBox.Show("Lütfen Hesaplama Yaptıktan Sonra Toplamını Çıkarınız.", "Sistem");
+
+            }
+            else {
+                double Persones1, Persones2, Persones3, Normal;
+                double VergiKesintisiPer1, VergiKesintisiPer2, VergiKesintisiPer3;
+                double GuncelMaasPer1, GuncelMaasPer2, GuncelMaasPer3, Gtoplam;
+                string NormalYaziyaCev, Nyazi;
+                string YaziyaCev, Gsonuc;
+
+                Persones1 = Double.Parse(Person1.Text);
+                Persones2 = Double.Parse(Person2.Text);
+                Persones3 = Double.Parse(Person3.Text);
+
+                Normal = Persones1 + Persones2 + Persones3;
+                NormalYaziyaCev = Convert.ToString(Normal);
+                Nyazi = normalMaaslar.Text = NormalYaziyaCev;
+
+                VergiKesintisiPer1 = Persones1 * 15 / 100;
+                GuncelMaasPer1 = Persones1 - VergiKesintisiPer1;
+
+                VergiKesintisiPer2 = Persones2 * 15 / 100;
+                GuncelMaasPer2 = Persones2 - VergiKesintisiPer2;
+
+                VergiKesintisiPer3 = Persones3 * 15 / 100;
+                GuncelMaasPer3 = Persones3 - VergiKesintisiPer3;
+
+                Gtoplam = GuncelMaasPer1 + GuncelMaasPer2 + GuncelMaasPer3;
+
+                YaziyaCev = Convert.ToString(Gtoplam);
+                Gsonuc = toplami.Text = YaziyaCev;
+            }
+
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Github: 31000s
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        //PERSON 1
+        {
+            
+            if (textBox1.Text == "" || textBox2.Text == "")
+            {
+                MessageBox.Show("Ad ve Soyad Alanı Boş", "Person 1");
+            }
+            else if (Person1.Text == "")
+            {
+
+                MessageBox.Show("Lütfen maaş giriniz.", "Person 1");
+            }
+            else
+            {
+                
+                double PersonCash1 = Double.Parse(Person1.Text);
+                double vergiKesintisi1 = 0, GuncelMaas1 = 0;
+                vergiKesintisi1 = PersonCash1 * 15 / 100;
+                GuncelMaas1 = PersonCash1 - vergiKesintisi1;
+                string YaziyaCev1 = Convert.ToString(GuncelMaas1);
+                string sonuc1 = maasl1.Text = YaziyaCev1;
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //PERSON 2
+            if (textBox3.Text == "" || textBox4.Text == "")
+            {
+                MessageBox.Show("Ad ve Soyad Alanı Boş", "Person 2");
+            }
+            else if (Person2.Text == "")
+            {
+
+                MessageBox.Show("Lütfen maaş giriniz.", "Person 2");
+            }
+            else
+            {
+                double PersonCash2 = Double.Parse(Person2.Text);
+                double vergiKesintisi2 = 0, GuncelMaas2 = 0;
+                vergiKesintisi2 = PersonCash2 * 15 / 100;
+                GuncelMaas2 = PersonCash2 - vergiKesintisi2;
+                string YaziyaCev2 = Convert.ToString(GuncelMaas2);
+                string sonuc2 = maasl2.Text = YaziyaCev2;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //PERSON 2
+            if (textBox6.Text == "" || textBox5.Text == "")
+            {
+                MessageBox.Show("Ad ve Soyad Alanı Boş", "Person 3");
+            }
+            else if (Person3.Text == "")
+            {
+
+                MessageBox.Show("Lütfen maaş giriniz.", "Person 3");
+            }
+            else
+            {
+                double PersonCash3 = Double.Parse(Person3.Text);
+                double vergiKesintisi3 = 0, GuncelMaas3 = 0;
+                vergiKesintisi3 = PersonCash3 * 15 / 100;
+                GuncelMaas3 = PersonCash3 - vergiKesintisi3;
+                string YaziyaCev3 = Convert.ToString(GuncelMaas3);
+                string sonuc3 = maasl3.Text = YaziyaCev3;
+            }
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/31000s");
+        }
+
+    }
+
+        }
+
+    
+
+    
+
